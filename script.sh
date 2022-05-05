b8dc5ad15102aab8f2cbe96a72b3d27ad3b49c44
haxnel <amanoyuki442@gmail.com>
2022-05-05 15:03:42 +0700

    Belajar

--- script.sh
+++ script.sh
@@ -1,1 +1,21 @@
+#cek repo kontainer
+docker image ls | grep "nama repo"
+
+#FROM instruction
 docker build -t haxnel/from from
+
+docker image ls
+
+#RUN instruction
+docker build -t haxnel/run run
+docker build -t haxnel/run run --progress=plain --no-cache
+
+#COMMAND(CMD) instruction
+docker build -t haxnel/command command
+
+docker image inspect haxnel/command
+
+docker container create --name command haxnel/command
+docker container start command
+docker container logs command
+docker container ls -a | grep command
